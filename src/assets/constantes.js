9939996ffb4447c5c994229d0fce3a8ba4af928b
Aderauwu04 <aderabg1@gmail.com>
2023-03-29 00:43:15 -0400

    estructura de sesión iniciada

---
+++ src/assets/constantes.js
@@ -0,0 +1,18 @@
+const plantillas = [
+	{
+		name: 'Formulario en blanco',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
+		imageAlt:
+			'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+		href: '#',
+	},
+	{
+		name: 'Paleta Rosa',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
+	},
+	{
+		name: 'Paleta Azul',
+		imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
+	},
+];
+export { plantillas };
