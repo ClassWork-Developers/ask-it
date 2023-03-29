331f71ae23c359b9cd66655522bb0c38391133b5
victoriaQuintero <victoria170460@gmail.com>
2023-03-28 23:36:46 -0400

    crear form agregado y por editar

--- src/components/NavbarSesion.jsx
+++ src/components/NavbarSesion.jsx
@@ -261,7 +261,7 @@ export default function Nav() {
         </Disclosure>
       </div>
       <main>
-        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+        <div className="">
           <Outlet />
         </div>
       </main>
