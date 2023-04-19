9634d72d751e5a17f0bf80736372749a6be8b79a
victoriaQuintero <victoria170460@gmail.com>
2023-04-18 21:36:04 -0400

    Rutas protegisdas lista

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -1,15 +1,38 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function PrivateRoutes({children}){
-    let user = localStorage.getItem('currentUser');
-    user = JSON.parse(user);
-    console.log(user)
-    if(user !== null){
-        return children
-    }else{
-        return <Navigate to={'/'}/>
-    }
+function PrivateRoutes({ children }) {
+  const { isLoading, isAuthenticated } = useAuth0();
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("currentUser"))
+  );
+  console.log(user);
+
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem("currentUser")));
+  }, []);
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <Circles
+          height="80"
+          width="80"
+          color="gray"
+          ariaLabel="circles-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      </div>
+    );
+  }
+
+  if (user !== null || isAuthenticated) {
+    return children;
+  } else {
+    return <Navigate to={"/"} />;
+  }
 }
-
 
 export default PrivateRoutes;
